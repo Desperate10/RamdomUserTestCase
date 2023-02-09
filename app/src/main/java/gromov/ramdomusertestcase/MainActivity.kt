23f24eae7166aec451df418c0f14bae42ceb354e
Евгений Громов <gromov.zhenya@gmail.com>
2023-02-09 10:04:55 +0200

    initial

---
+++ app/src/main/java/gromov/ramdomusertestcase/MainActivity.kt
@@ -0,0 +1,11 @@
+package gromov.ramdomusertestcase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
