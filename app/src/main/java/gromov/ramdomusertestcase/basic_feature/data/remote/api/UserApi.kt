537d5be1c25debfc05dcb2b4f24fc554d3f331ee
Евгений Громов <gromov.zhenya@gmail.com>
2023-02-13 00:56:47 +0200

    Delete unused db table

--- app/src/main/java/gromov/ramdomusertestcase/basic_feature/data/remote/api/UserApi.kt
+++ app/src/main/java/gromov/ramdomusertestcase/basic_feature/data/remote/api/UserApi.kt
@@ -1,6 +1,7 @@
 package gromov.ramdomusertestcase.basic_feature.data.remote.api
 
 import gromov.ramdomusertestcase.basic_feature.data.remote.model.RandomUserResponse
+import kotlinx.coroutines.flow.Flow
 import retrofit2.http.GET
 
 interface UserApi {
